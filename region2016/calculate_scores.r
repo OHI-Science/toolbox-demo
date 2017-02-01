0a36df4c1f3f3abc535484e9daf3fa7a639470c4
jules32 <jstewart@nceas.ucsb.edu>
2017-01-31 16:24:01 -0800

    create toolbox-demo up to ohiwebapps::create_repo_map

---
+++ region2016/calculate_scores.r
@@ -0,0 +1,40 @@
+## calculate_scores.R 
+
+## calculate_scores.R ensures all files are properly configured and calculates OHI scores.
+  ## - configure_toolbox.r ensures your files are properly configured. It is a script in your repository.
+  ## - CalculateAll() calculates OHI scores. It is a function in the `ohicore` R package 
+  ##   (this can be written in R as `ohicore::CalculateAll()`).  
+
+## When you begin, configure_toolbox.r and CalculateAll() will calculate scores using
+## the 'templated' data and goal models provided. We suggest you work
+## goal-by-goal as you prepare data in the prep folder and develop goal models
+## in functions.r. Running configure_toolbox.r and a specific goal model line-by-line 
+## in functions.R is a good workflow.
+
+## run the configure_toolbox.r script to check configuration
+source('~/github/toolbox-demo/region2016/configure_toolbox.r')
+
+## calculate scenario scores
+scores = ohicore::CalculateAll(conf, layers)
+
+## save scores as scores.csv
+write.csv(scores, 'scores.csv', na='', row.names=F)
+
+
+## create figures ----
+source('PrepSpatial.r')  # until added to ohicore
+source('PlotMap.r')      # until added to ohicore
+source('PlotMapMulti.r') # until added to ohicore
+
+## Make Maps for each goal
+PlotMapMulti(scores       = scores,
+             spatial_poly = PrepSpatial('spatial/regions_gcs.geojson'),
+             path_figures = 'reports/figures')
+
+
+## Make Flower Plots for each region
+PlotFlowerMulti(scores          = scores, 
+                rgns_to_plot    = unique(scores$region_id), 
+                assessment_name = 'Global')
+
+
---
+++ region2016/configure_toolbox.r
@@ -0,0 +1,31 @@
+## configure_repo.r
+
+## configure_repo.r ensures all files in your repo are properly configured. 
+## It must be sourced before calculating OHI scores with ohicore::CalculateAll(); 
+## it can be sourced here or is also sourced from calculate_scores.r.
+
+## You are encouraged to use this script when developing individual goal models. A good workflow is: 
+  ## 1. prepare data layers in the /prep folders (script as much as possible in R)
+  ## 2. register data layers in layers.csv and save them in /layers folder
+  ## 3. source configure_repo.r to ensure proper configuration
+  ## 4. develop goal models in functions.r, running individual goal models line by line
+
+## load ohicore and tidyverse (includes dplyr, tidyr, stringr, etc)
+if (!"ohicore" %in% (.packages())) {
+  suppressWarnings(require(ohicore))
+  library(tidyverse)    # install.packages('tidyverse')
+  library(stringr)
+}
+
+## set working directory to the scenario that contains conf and layers directories
+setwd('~/github/toolbox-demo/region2016')
+
+## load scenario configuration
+conf = ohicore::Conf('conf')
+
+## check that scenario layers files in the \layers folder match layers.csv registration. Layers files are not modified.
+ohicore::CheckLayers('layers.csv', 'layers', flds_id=conf$config$layers_id_fields)
+
+## load scenario layers for ohicore to access. Layers files are not modified.
+layers = ohicore::Layers('layers.csv', 'layers')
+
