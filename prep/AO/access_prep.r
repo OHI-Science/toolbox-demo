6c7b5592c1e0db3ec4522c957eb15266ee48547e
jules32 <jstewart@nceas.ucsb.edu>
2018-07-18 10:19:57 -0700

    update access_prep.r for 2017

--- prep/AO/access_prep.r
+++ prep/AO/access_prep.r
@@ -1,16 +1,17 @@
 ## access_prep.r
 
-## In this example we will prepare local data that will substitute Global OHI data for the data layer `ao_access`. The 'local data' will be dummy data in this example. We will import data that is already formatted as a data layer for the Toolbox, save it in the "layers" folder, and finally make sure it is properly registered in "layers.csv".
+## In this example we will prepare local data that will substitute Global OHI data for the data layer `ao_access`. The 'local data' will be dummy data in this example. We will import data that is already formatted as a data layer for the Toolbox, save it in the "layers" folder, and finally make sure it is properly registered in `layers.csv` and `scenario_data_years.csv`.
 
 ## ***************************************************************************** ##
 
 ## setup: libraries, file paths ----
 library(tidyverse) # install.packages('tidyverse')
-dir_layers <- file.path('~/github/toolbox-demo/region2016/layers')
+library(here) # install.packages('here')
+dir_layers <- here::here('region2017/layers')
 
 
 ## import dummy 'local data' that is already formatted nicely. Note the naming convention of the data file: it is "goalcode_layername_assessmentYEAR.csv".
-data_file  <- file.path(dir_layers, 'ao_access_gl2016.csv')
+data_file  <- file.path(dir_layers, 'ao_access_gl2017.csv')
 d <- readr::read_csv(data_file)
 
 ## look at a summary to see the range of values
